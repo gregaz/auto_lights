807dacbe93778ebc54575cb8db26ef0a84526830
gregory <greg.azuolas@gmail.com>
2016-07-31 16:52:59 -0400

    add config file back

---
+++ config.py
@@ -0,0 +1,15 @@
+
+#Complete these variables before installing onto raspberry pi
+#if you fork this project to a public repo, you might want to add this file to .gitignore so that you do no share these details
+
+pi_mac = ''
+pi_user = ''
+pi_password = ''
+
+light_labels = ['']
+wake_time = 0
+shutoff_delay = 120
+
+phone_macs = []
+
+log_file = '/tmp/auto_lights.log'
