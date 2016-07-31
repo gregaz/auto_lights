2648175321efff3b16bf70f60dc5a01d71067bd2
gregory <greg.azuolas@gmail.com>
2016-07-31 16:57:31 -0400

    fixing config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-#test
+
+#Complete these variables before installing onto raspberry pi
+#if you fork this project to a public repo, you might want to add this file to .gitignore so that you do no share these details
 
 pi_mac = ''
 pi_user = ''
