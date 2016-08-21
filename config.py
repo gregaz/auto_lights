1ca2e13efa6e07cdd216037b4c7fcea384d14018
gregory <greg.azuolas@gmail.com>
2016-08-21 12:28:23 -0400

    added option to turn on or off certain lights

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-#test 2
-pi_mac = ''
+
+pi_mac = '00:e0:4c:36:56:8b'
 pi_user = ''
 pi_password = ''
 
-light_labels = ['']
+light_labels_to_power_off = ['']
+light_labels_to_power_on = ['']
 wake_time = 0
 shutoff_delay = 120
 
--- power_lights.py
+++ power_lights.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 from lifxlan import *
-from config import light_labels
+from config import light_labels_to_power_on, light_labels_to_power_off
 import sys
 
 lifx = LifxLAN()
 devices = lifx.get_lights()
-bulbs_to_power_on = [bulb for bulb in devices if bulb.get_label() in light_labels]
-for bulb in bulbs_to_power_on:
+
+if (sys.argv[1] == 'on'):
+    light_labels = light_labels_to_power_on
+else:
+    light_labels = light_labels_to_power_off
+
+bulbs_to_power = [bulb for bulb in devices if bulb.get_label() in light_labels]
+for bulb in bulbs_to_power:
     bulb.set_power(sys.argv[1])
